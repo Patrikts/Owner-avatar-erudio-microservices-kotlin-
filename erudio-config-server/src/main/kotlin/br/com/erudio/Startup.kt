46353cc720cc60b911887187f310e36e4c692acf
Vinicius Teixeira Saraiva <trikteixeira@gmail.com>
2023-09-02 14:28:08 -0300

    Criando um Projeto Spring Cloud Config Server Local

--- erudio-config-server/src/main/kotlin/br/com/erudio/Startup.kt
+++ erudio-config-server/src/main/kotlin/br/com/erudio/Startup.kt
@@ -2,8 +2,10 @@ package br.com.erudio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class Startup
 
 fun main(args: Array<String>) {
