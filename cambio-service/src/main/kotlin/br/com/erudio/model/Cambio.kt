a962c0a1f87c010886890a129590f7d12d501c6a
Vinicius Teixeira Saraiva <trikteixeira@gmail.com>
2023-09-06 11:41:08 -0300

    Adicionando as Annotations do JPA e Integrando o Serviço ao Banco de Dados

--- cambio-service/src/main/kotlin/br/com/erudio/model/Cambio.kt
+++ cambio-service/src/main/kotlin/br/com/erudio/model/Cambio.kt
@@ -1,13 +1,32 @@
 package br.com.erudio.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Transient
 import java.math.BigDecimal
 
+@Entity(name = "cambio")
 data class Cambio (
-    var id: Long = 0,
-    var from: String = "",
-    var to: String = "",
-    var conversionFactor: BigDecimal = BigDecimal.ZERO,
-    var convertedValue: BigDecimal = BigDecimal.ZERO,
-    var environment: String = "",
 
-    )
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(name = "from_currency", nullable = false, length = 3)
+    var from: String = "",
+
+    @Column(name = "to_currency", nullable = false, length = 3)
+    var to: String = "",
+
+    @Column(nullable = false)
+    var conversionFactor: BigDecimal = BigDecimal.ZERO,
+
+    @Transient
+    var convertedValue: BigDecimal = BigDecimal.ZERO,
+
+    @Transient
+    var environment: String? = ""
+)
