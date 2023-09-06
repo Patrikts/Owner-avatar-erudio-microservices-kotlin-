9dfb6bf3eb715ccbdb9dac2531f41b75d3c729ab
Vinicius Teixeira Saraiva <trikteixeira@gmail.com>
2023-09-06 09:01:01 -0300

    Criando o Microserviço Cambio Service

--- greeting-service/src/main/kotlin/br/com/erudio/config/GreetingConfiguration.kt
+++ greeting-service/src/main/kotlin/br/com/erudio/config/GreetingConfiguration.kt
@@ -1,10 +1,12 @@
 package br.com.erudio.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.stereotype.Component
 
 @Component
 @ConfigurationProperties("greeting-service")
+@RefreshScope
 class GreetingConfiguration {
     var greeting: String? = null
     var defaultValue: String? = null
