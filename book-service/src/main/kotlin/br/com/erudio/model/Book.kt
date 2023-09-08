afc23c862618af213988b0728baefb07512c3a57
Vinicius Teixeira Saraiva <trikteixeira@gmail.com>
2023-09-08 15:35:48 -0300

    Conclusão da Seção - Criando e Conectando Microserviços com Feign

---
+++ book-service/src/main/kotlin/br/com/erudio/model/Book.kt
@@ -0,0 +1,25 @@
+package br.com.erudio.model
+
+import jakarta.persistence.*
+import java.util.Date
+
+@Entity(name = "book")
+data class Book (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+    @Column(name = "author", nullable = false, length = 180)
+    var author: String = "",
+    @Column(name = "title", nullable = false, length = 250)
+    var title: String = "",
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    var launchDate: Date? = null,
+    @Column(name = "price", nullable = false)
+    var price: Double? = null,
+    @Transient
+    var currency: String = "",
+    @Transient
+    var environment: String? = ""
+    )
